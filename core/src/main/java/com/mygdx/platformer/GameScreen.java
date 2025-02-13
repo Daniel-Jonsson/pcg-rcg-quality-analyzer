9481c5dd040a0c45acb9a7356b2196472680f372
Robert Kullman <roku0701@student.miun.se>
2025-02-13 16:42:06 +0100

    Configure game loop
    - Configured the GameScreen class to extend ScreenAdapter.
    - Configured
    - Added update() method for logic updates.
    - Added test graphics to verify that rendering works as expected

--- core/src/main/java/com/mygdx/platformer/GameScreen.java
+++ core/src/main/java/com/mygdx/platformer/GameScreen.java
@@ -1,17 +1,88 @@
 package com.mygdx.platformer;
 
-import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GameScreen extends ScreenAdapter {
+
+   private PlatformerGame game;
+
+   private Sprite sprite;
+
+    private SpriteBatch batch;
+    private OrthographicCamera camera;
+    private Texture texture;        // for graphics test
+
+    private float runTime; // tracks how long the game has run
+    
+
+   public GameScreen(PlatformerGame game) {
+       this.game = game; // reference main class to enable switching to another screen
+
+        Gdx.app.log(this.getClass().getSimpleName(), "Loaded");
+   }
+
+   private void createTestGraphics() {
+       int width = 50;
+       int height = 50;
+
+       Pixmap pixmap = new Pixmap(width, height, Pixmap.Format.RGBA8888);
+       pixmap.setColor(Color.RED);
+       pixmap.fill();
+       pixmap.drawRectangle(0, 0, width, height);
+
+       texture = new Texture(pixmap);
+       sprite = new Sprite(texture);
+       sprite.setPosition(100, 100);
+       sprite.setSize(width, height);
 
 
-public class GameScreen implements Screen {
+   }
+
+    // Executes when this screen is set as the active screen
     @Override
     public void show() {
-        // Prepare your screen here.
+        Gdx.app.log(this.getClass().getSimpleName(), "Show");
+
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false); // invert coordinates (y = 0 at bottom of window)
+
+        createTestGraphics();
+
     }
 
+    // updates logic
+    private void update(float deltaTime) {
+        runTime += deltaTime;
+        sprite.translate(1, 0);
+    }
+
+    // updated logics, graphics etc. Equivalent to game loop.
     @Override
-    public void render(float delta) {
-        // Draw your screen here. "delta" is the time since last render in seconds.
+    public void render(float deltaTime) {
+        // buffer screen
+        Gdx.gl20.glClearColor(0, 0, 0, 1);
+        Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.setProjectionMatrix(camera.combined);
+
+        // update here
+        update(deltaTime);
+        sprite.rotate(1);
+        sprite.setOrigin(sprite.getWidth() / 2, sprite.getHeight() / 2);
+
+        batch.begin();  // render
+        sprite.draw(batch);
+        batch.end();
+
     }
 
     @Override
@@ -19,19 +90,23 @@ public class GameScreen implements Screen {
         // Resize your screen here. The parameters represent the new window size.
     }
 
+    // executes when the game loses focus, e.g. when minimized
     @Override
     public void pause() {
-        // Invoked when your application is paused.
+
+        Gdx.app.log(this.getClass().getSimpleName(), "Paused");
     }
 
+    // executed when game regains focus, i.e. when it is brought to the foreground.
     @Override
     public void resume() {
-        // Invoked when your application is resumed after pause.
+        Gdx.app.log(this.getClass().getSimpleName(), "Resumed");
     }
 
     @Override
     public void hide() {
         // This method is called when another screen replaces this one.
+        Gdx.app.log(this.getClass().getSimpleName(), "Hide");
     }
 
     @Override
--- core/src/main/java/com/mygdx/platformer/PlatformerGame.java
+++ core/src/main/java/com/mygdx/platformer/PlatformerGame.java
@@ -4,8 +4,17 @@ import com.badlogic.gdx.Game;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class PlatformerGame extends Game {
+
+    /**
+     * Entry point of application.
+     */
     @Override
     public void create() {
-        setScreen(new GameScreen());
+        setScreen(new GameScreen(this));
+    }
+
+    @Override
+    public void dispose() {
+         super.dispose();
     }
 }

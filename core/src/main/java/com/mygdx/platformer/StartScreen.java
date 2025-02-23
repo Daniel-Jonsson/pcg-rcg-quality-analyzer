75825f55a778989ccf4dd4f2384dead926d796da
Robert Kullman <roku0701@student.miun.se>
2025-02-23 14:38:01 +0100

    Add necessary asset files

--- core/src/main/java/com/mygdx/platformer/StartScreen.java
+++ core/src/main/java/com/mygdx/platformer/StartScreen.java
@@ -1,8 +1,24 @@
 package com.mygdx.platformer;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class StartScreen implements Screen {
+    private Game game;
+    private Skin skin;
+    private Stage stage;
+
+    public StartScreen(Game game) {
+        this.game = game;
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        this.skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
+    }
+
     @Override
     public void show() {
 

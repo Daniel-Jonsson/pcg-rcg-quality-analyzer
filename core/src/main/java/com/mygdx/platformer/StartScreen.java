c29b4f7a13a95b92bf7eb852d0e4a42e78fcdc6c
Robert Kullman <roku0701@student.miun.se>
2025-02-23 14:53:54 +0100

    Add "Start Game" button to StartScreen.java

--- core/src/main/java/com/mygdx/platformer/StartScreen.java
+++ core/src/main/java/com/mygdx/platformer/StartScreen.java
@@ -3,20 +3,40 @@ package com.mygdx.platformer;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class StartScreen implements Screen {
-    private Game game;
+    private PlatformerGame game;
     private Skin skin;
     private Stage stage;
 
-    public StartScreen(Game game) {
+    public StartScreen(PlatformerGame game) {
         this.game = game;
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
 
         this.skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
+
+        TextButton startButton = new TextButton("Start Game", skin);
+        startButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new GameScreen(game));
+            }
+        });
+
+        // Arrange UI with Table
+        Table table = new Table();
+        table.setFillParent(true);
+        table.add(startButton).width(250).height(50);
+
+        stage.addActor(table);
     }
 
     @Override
@@ -26,12 +46,14 @@ public class StartScreen implements Screen {
 
     @Override
     public void render(float delta) {
-
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
@@ -51,6 +73,7 @@ public class StartScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        stage.dispose();
+        skin.dispose();
     }
 }

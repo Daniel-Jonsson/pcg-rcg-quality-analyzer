a45f6f1481b1b5c3afbe3012dae41d6bca652e42
Robert Kullman <roku0701@student.miun.se>
2025-02-23 14:59:54 +0100

    Extracted magic numbers to AppConfig and added initial documentation comments.

--- core/src/main/java/com/mygdx/platformer/StartScreen.java
+++ core/src/main/java/com/mygdx/platformer/StartScreen.java
@@ -1,6 +1,5 @@
 package com.mygdx.platformer;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
@@ -10,12 +9,24 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.mygdx.platformer.utilities.AppConfig;
 
+/**
+ * This class represents the starting screen of the game, where the main game menu resides.
+ *
+ * @author Robert Kullman
+ * @author Daniel Jönsson
+ */
 public class StartScreen implements Screen {
     private PlatformerGame game;
     private Skin skin;
     private Stage stage;
 
+    /**
+     * Constructor for the StartScreen, which initializes the UI elements.
+     *
+     * @param game The main game instance used to switch screens.
+     */
     public StartScreen(PlatformerGame game) {
         this.game = game;
         stage = new Stage();
@@ -34,16 +45,24 @@ public class StartScreen implements Screen {
         // Arrange UI with Table
         Table table = new Table();
         table.setFillParent(true);
-        table.add(startButton).width(250).height(50);
+        table.add(startButton).width(AppConfig.BUTTON_WIDTH).height(AppConfig.BUTTON_HEIGHT);
 
         stage.addActor(table);
     }
 
+    /**
+     * Called when the screen is first shown.
+     */
     @Override
     public void show() {
 
     }
 
+    /**
+     * Renders the screen by clearing the background and drawing UI elements.
+     *
+     * @param delta Time elapsed since the last frame.
+     */
     @Override
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -51,26 +70,44 @@ public class StartScreen implements Screen {
         stage.draw();
     }
 
+    /**
+     * Called when the screen is resized.
+     *
+     * @param width  The new width of the screen.
+     * @param height The new height of the screen.
+     */
     @Override
     public void resize(int width, int height) {
         stage.getViewport().update(width, height, true);
     }
 
+    /**
+     * Called when the game is paused (e.g., when the application is minimized).
+     */
     @Override
     public void pause() {
 
     }
 
+    /**
+     * Called when the game is resumed from a paused state.
+     */
     @Override
     public void resume() {
 
     }
 
+    /**
+     * Called when this screen is no longer the active screen.
+     */
     @Override
     public void hide() {
 
     }
 
+    /**
+     * Disposes of resources when the screen is no longer needed.
+     */
     @Override
     public void dispose() {
         stage.dispose();

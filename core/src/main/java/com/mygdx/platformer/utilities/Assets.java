59535afa0d234ad6b2a0894538f802feba6469dd
Robert Kullman <roku0701@student.miun.se>
2025-03-02 13:42:08 +0100

    add textures to asset manager.

--- core/src/main/java/com/mygdx/platformer/utilities/Assets.java
+++ core/src/main/java/com/mygdx/platformer/utilities/Assets.java
@@ -12,10 +12,18 @@ public final class Assets {
     public static final String PLAYER_TEXTURE = "textures/player.png";
     public static final String PLATFORM_TEXTURE = "textures/platform.png";
     public static final String THROWING_DAGGER_TEXTURE = "textures/throwing_dagger.png";
+    public static final String GOBLIN_IDLE = "textures/goblin_idle/goblin_idle.png";
+    public static final String NECROMANCER_1 = "textures/necromancer_idle_and_walk/necromancer1.png";
 
     public static void load() {
         assetManager.load(PLAYER_TEXTURE, Texture.class);
         assetManager.load(PLATFORM_TEXTURE, Texture.class);
+        assetManager.load(THROWING_DAGGER_TEXTURE, Texture.class);
+        assetManager.load(GOBLIN_IDLE, Texture.class);
+        assetManager.load(NECROMANCER_1, Texture.class);
+
+
+
         assetManager.update();
         assetManager.finishLoading();  // block to make sure the manager has finished loading
     }

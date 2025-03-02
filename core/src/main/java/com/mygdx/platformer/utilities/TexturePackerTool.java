35da3c33107b89f84c301c185c3c0d9209d910ce
Robert Kullman <roku0701@student.miun.se>
2025-03-02 19:32:41 +0100

    Created sprite atlas for Necromancer animations, and added atlases to asset manager.

--- core/src/main/java/com/mygdx/platformer/utilities/TexturePackerTool.java
+++ core/src/main/java/com/mygdx/platformer/utilities/TexturePackerTool.java
@@ -5,9 +5,9 @@ import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 public class TexturePackerTool {
     public static void main(String[] args) {
         TexturePacker.process(
-            "assets/goblin_sprites",  // Input folder (contains images)
+            "assets/necromancer_sprites",  // Input folder (contains images)
             "assets/atlas",           // Output folder (where atlas will be saved)
-            "goblin_sprites"          // Atlas filename
+            "necromancer_sprites"          // Atlas filename
         );
         System.out.println("Texture packing complete!");
     }

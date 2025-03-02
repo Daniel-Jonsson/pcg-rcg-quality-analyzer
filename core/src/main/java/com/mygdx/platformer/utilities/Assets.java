d03b12a1421a4a878f0d4fc5fb4218f3a9bcff03
Robert Kullman <roku0701@student.miun.se>
2025-03-02 14:28:06 +0100

    reorganized player sprites and constructed player sprite atlas.

--- core/src/main/java/com/mygdx/platformer/utilities/Assets.java
+++ core/src/main/java/com/mygdx/platformer/utilities/Assets.java
@@ -2,30 +2,33 @@ package com.mygdx.platformer.utilities;
 
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public final class Assets {
 
-    private Assets() { }  // private constructor to prevent instantiation
+    private Assets() { }  // Private constructor to prevent instantiation
 
     public static final AssetManager assetManager = new AssetManager();
 
+    public static final String PLAYER_ATLAS = "atlas/player_sprites.atlas";
     public static final String PLAYER_TEXTURE = "textures/player.png";
-    public static final String PLATFORM_TEXTURE = "textures/platform.png";
     public static final String THROWING_DAGGER_TEXTURE = "textures/throwing_dagger.png";
     public static final String GOBLIN_IDLE = "textures/goblin_idle/goblin_idle.png";
-    public static final String NECROMANCER_1 = "textures/necromancer_idle_and_walk/necromancer1.png";
+    public static final String PLATFORM_TEXTURE = "textures/platform.png";
 
     public static void load() {
+        assetManager.load(PLAYER_ATLAS, TextureAtlas.class);
         assetManager.load(PLAYER_TEXTURE, Texture.class);
-        assetManager.load(PLATFORM_TEXTURE, Texture.class);
         assetManager.load(THROWING_DAGGER_TEXTURE, Texture.class);
         assetManager.load(GOBLIN_IDLE, Texture.class);
-        assetManager.load(NECROMANCER_1, Texture.class);
-
-
+        assetManager.load(PLATFORM_TEXTURE, Texture.class);
 
         assetManager.update();
-        assetManager.finishLoading();  // block to make sure the manager has finished loading
+        assetManager.finishLoading();  // Block to ensure assets are loaded
+    }
+
+    public static TextureAtlas getPlayerAtlas() {
+        return assetManager.get(PLAYER_ATLAS, TextureAtlas.class);
     }
 
     public static void dispose() {

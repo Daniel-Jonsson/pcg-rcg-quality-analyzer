8fdb6bdadb8b8319c487a580de1abb98920fe3c0
Daniel Jönsson <dajo1903@student.miun.se>
2025-03-03 13:04:12 +0100

    Removed unused imports.
    Signed-off-by: Daniel Jönsson <dajo1903@student.miun.se>

--- core/src/main/java/com/mygdx/platformer/EnemyManager.java
+++ core/src/main/java/com/mygdx/platformer/EnemyManager.java
@@ -1,6 +1,5 @@
 package com.mygdx.platformer;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
--- core/src/main/java/com/mygdx/platformer/characters/enemies/BaseEnemy.java
+++ core/src/main/java/com/mygdx/platformer/characters/enemies/BaseEnemy.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.mygdx.platformer.utilities.AppConfig;
 
-import java.util.Iterator;
 
 public abstract class BaseEnemy {
 

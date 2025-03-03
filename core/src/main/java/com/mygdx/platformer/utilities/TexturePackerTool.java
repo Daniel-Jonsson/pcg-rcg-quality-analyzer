9d9d17bf74f4610c0ff70a7dc6500cc3368305df
Daniel Jönsson <dajo1903@student.miun.se>
2025-03-03 21:06:13 +0100

    Added Javadoc for TexturePackerTool.java
    Signed-off-by: Daniel Jönsson <dajo1903@student.miun.se>

--- core/src/main/java/com/mygdx/platformer/utilities/TexturePackerTool.java
+++ core/src/main/java/com/mygdx/platformer/utilities/TexturePackerTool.java
@@ -2,6 +2,14 @@ package com.mygdx.platformer.utilities;
 
 import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 
+/**
+ * Utility class for packing textures into a texture atlas using LibGDX's
+ * {@link TexturePacker} This tool processes individual sprite images and
+ * generates a texture atlas, which is used for optimized rendering in the
+ * game.
+ *
+ * @author Daniel Jönsson, Robert Kullman
+ */
 public class TexturePackerTool {
     public static void main(String[] args) {
         TexturePacker.process(

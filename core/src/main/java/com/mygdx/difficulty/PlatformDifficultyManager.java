20e47d13b3fe6abc9653ead033f922dc35bebf75
Daniel Jönsson <dajo1903@student.miun.se>
2025-03-12 17:41:13 +0100

    Made PlatformDifficultyManager for managing platform difficulty.

---
+++ core/src/main/java/com/mygdx/difficulty/PlatformDifficultyManager.java
@@ -0,0 +1,11 @@
+package com.mygdx.difficulty;
+
+/**
+ * This class is responsible for managing the difficulty of the platforms. The difficulty is related to the width of the platforms, distance between them and the difference in height between two platforms.
+ * 
+ * @author Robert Kullman
+ * @author Daniel Jönsson
+ */
+public class PlatformDifficultyManager extends DifficultyManager {
+    
+}

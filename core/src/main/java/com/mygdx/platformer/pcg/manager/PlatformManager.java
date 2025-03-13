39432eb7823c1bea5ed8de38bd369c3f714faa6a
Daniel Jönsson <dajo1903@student.miun.se>
2025-03-13 11:51:19 +0100

    Made PlatformManager which will manage platform generations and lifecycle.

---
+++ core/src/main/java/com/mygdx/platformer/pcg/manager/PlatformManager.java
@@ -0,0 +1,14 @@
+package com.mygdx.platformer.pcg.manager;
+
+/**
+ * Manages platform generation and lifecycle.
+ * This class is responsible for creating, updating, rendering, and disposing
+ * platforms.
+ * It uses an IPlatformGenerator to generate new platforms.
+ */
+public class PlatformManager {
+    
+    public PlatformManager() {
+       
+    }
+}

827445ea4164f01dbb6a1ec923007e2caf6b3f03
Daniel Jönsson <dajo1903@student.miun.se>
2025-03-13 11:46:04 +0100

    Made interface for platform generator(s)

---
+++ core/src/main/java/com/mygdx/platformer/pcg/generators/IPlatformGenerator.java
@@ -0,0 +1,17 @@
+package com.mygdx.platformer.pcg.generators;
+
+import com.badlogic.gdx.physics.box2d.World;
+import com.mygdx.platformer.EnemyManager;
+import com.mygdx.platformer.pcg.Platform;
+
+/**
+ * Interface for platform generators. Implementations of this interface
+ * are responsible for generating platforms with different characteristics.
+ * 
+ * @author Robert Kullman
+ * @author Daniel Jönsson
+ */
+public interface IPlatformGenerator {
+    Platform initialize(World world, EnemyManager enemyManager);
+    Platform generatePlatform(float lastPlatformX, float baseY);
+}

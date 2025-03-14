fa20a99fa5e0d8bba2209eb5b342413c0e6a16f2
Daniel Jönsson <dajo1903@student.miun.se>
2025-03-14 14:58:09 +0100

    Implemented initialization method for ErrorOverlay to set up stage and skin resources.

--- core/src/main/java/com/mygdx/platformer/screens/overlays/ErrorOverlay.java
+++ core/src/main/java/com/mygdx/platformer/screens/overlays/ErrorOverlay.java
@@ -1,9 +1,11 @@
 package com.mygdx.platformer.screens.overlays;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 /**
  * A static utility class for displaying error messages in the game.
@@ -18,4 +20,12 @@ public class ErrorOverlay {
     private static Skin skin;
     private static boolean isActive = false;
     private static InputProcessor previousInputProcessor;
+
+    /**
+     * Initializes the error overlay. This should be called once during the game initialization.
+     */
+    public static void initialize() {
+        stage = new Stage(new ScreenViewport());
+        skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
+    }
 }

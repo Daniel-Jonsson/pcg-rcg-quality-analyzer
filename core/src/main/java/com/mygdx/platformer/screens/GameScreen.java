a31d19fb5cfa9980da74d1cfe69d1a541927970e
Robert Kullman <roku0701@student.miun.se>
2025-03-15 00:27:22 +0100

    Add background music.
    This adds methods to AudioManager for playing, stopping, and setting the volume of the background music track.

--- core/src/main/java/com/mygdx/platformer/screens/GameScreen.java
+++ core/src/main/java/com/mygdx/platformer/screens/GameScreen.java
@@ -27,6 +27,7 @@ import com.mygdx.platformer.characters.player.Player;
 import com.mygdx.platformer.difficulty.GameDifficultyManager;
 import com.mygdx.platformer.pcg.manager.PlatformManager;
 import com.mygdx.platformer.screens.overlays.GameOverOverlay;
+import com.mygdx.platformer.sound.AudioManager;
 import com.mygdx.platformer.utilities.AppConfig;
 
 /**
@@ -126,6 +127,8 @@ public class GameScreen extends ScreenAdapter {
         gameOverOverlay = new GameOverOverlay(game, gameTimer.getElapsedTime());
 
         initCollisionListener();
+
+        AudioManager.playBackgroundMusic();
 
     }
 
--- core/src/main/java/com/mygdx/platformer/sound/AudioManager.java
+++ core/src/main/java/com/mygdx/platformer/sound/AudioManager.java
@@ -1,4 +1,27 @@
 package com.mygdx.platformer.sound;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+
 public class AudioManager {
+    private static Music backgroundMusic;
+
+    public static void playBackgroundMusic() {
+        backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("sound/background.mp3"));
+        backgroundMusic.setLooping(true);
+        backgroundMusic.setVolume(0.2f);
+        backgroundMusic.play();
+    }
+
+    public static void stopBackgroundMusic() {
+        backgroundMusic.stop();
+    }
+
+    public static void setMusicVolume(float volume) {
+        backgroundMusic.setVolume(volume);
+    }
+
+    public void dispose() {
+        backgroundMusic.dispose();
+    }
 }

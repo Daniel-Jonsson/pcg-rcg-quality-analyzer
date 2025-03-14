ae29fc6d17937068f114f298dd58dab0d1820539
Daniel Jönsson <dajo1903@student.miun.se>
2025-03-14 14:55:30 +0100

    Started working on ErrorOverlay. It can be used anywhere in the game by calling its static methods.

---
+++ core/src/main/java/com/mygdx/platformer/screens/overlays/ErrorOverlay.java
@@ -0,0 +1,13 @@
+package com.mygdx.platformer.screens.overlays;
+
+
+/**
+ * A static utility class for displaying error messages in the game.
+ * This overlay can be shown from anywhere in the game by calling the static methods.
+ * 
+ * @author Daniel Jönsson
+ * @author Robert Kullman
+ */
+public class ErrorOverlay {
+    
+}

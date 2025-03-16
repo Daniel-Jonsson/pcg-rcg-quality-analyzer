a3678f249689191e9c6c7cf96c6bfe77b6cab345
Daniel Jönsson <dajo1903@student.miun.se>
2025-03-16 15:46:56 +0100

    feat: Added SoundType enum
    Added enum class to define different types of sounds.

---
+++ core/src/main/java/com/mygdx/platformer/sound/SoundType.java
@@ -0,0 +1,11 @@
+package com.mygdx.platformer.sound;
+
+public enum SoundType {
+    SWOOSH,
+    SWOOSH2,
+    DEATHBOLT,
+    BUTTONHOVER,
+    BUTTONCLICK,
+    CHECKBOXCLICK,
+    SLIDERCHANGE
+}

7f72b513003068d7614de2e9b54a819f7f9278f7
Daniel Jönsson <dajo1903@student.miun.se>
2025-03-16 16:02:05 +0100

    refactor: Removed unused constructor in GoblinAttack.

--- core/src/main/java/com/mygdx/platformer/attacks/GoblinAttack.java
+++ core/src/main/java/com/mygdx/platformer/attacks/GoblinAttack.java
@@ -13,27 +13,6 @@ import com.mygdx.platformer.utilities.Assets;
  */
 public class GoblinAttack extends BaseAttack {
 
-    /**
-     * Creates an PlayerAttack instance that moves in the specified direction.
-     *
-     * @param world The Box2D world where the attack exists.
-     * @param x The initial x-coordinate of the attack.
-     * @param y The initial y-coordinate of the attack.
-     * @param texture The texture representing the attack's appearance.
-     * @param directionModifier The direction in which the attack moves (e.g.,
-     *                          -1 for left, 1 for right).
-     * @param isPlayerAttack Whether the attack is a player attack.
-     */
-    public GoblinAttack(World world, float x, float y, Texture texture, int directionModifier, boolean isPlayerAttack) {
-        super(world, AppConfig.GOBLIN_ATTACK_POWER,
-            AppConfig.GOBLIN_ATTACK_SPEED * directionModifier,
-            x + AppConfig.GOBLIN_ATTACK_X_OFFSET,
-            y + AppConfig.GOBLIN_ATTACK_Y_OFFSET,
-            texture,
-            isPlayerAttack);
-        super.body.setUserData(this);
-    }
-
     public GoblinAttack(World world, float x, float y, int directionModifier,
                         int dmg, float speed) {
         super(world, dmg, speed * directionModifier,

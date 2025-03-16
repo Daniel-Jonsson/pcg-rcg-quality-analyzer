c163f73df9db38dba1a1d2abb152bf3ea69806be
Daniel Jönsson <dajo1903@student.miun.se>
2025-03-16 20:05:43 +0100

    refactor: Removed wildcard import from ui package.

--- core/src/main/java/com/mygdx/platformer/screens/SettingsScreen.java
+++ core/src/main/java/com/mygdx/platformer/screens/SettingsScreen.java
@@ -8,7 +8,11 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Slider;
+import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -27,11 +31,10 @@ import com.mygdx.platformer.ui.GameButton;
  * @author Robert Kullman, Daniel Jönsson
  */
 public class SettingsScreen extends ScreenAdapter {
-    private final PlatformerGame game;
-    private Stage stage;
-    private Skin skin;
+    private final Stage stage;
+    private final Skin skin;
 
-    private GameTimer timerPreview;
+    private final GameTimer timerPreview;
 
     private SpriteBatch batch;
     private Texture healthBarTexture;
@@ -42,7 +45,6 @@ public class SettingsScreen extends ScreenAdapter {
      * @param game The game instance.
      */
     public SettingsScreen(final PlatformerGame game) {
-        this.game = game;
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 

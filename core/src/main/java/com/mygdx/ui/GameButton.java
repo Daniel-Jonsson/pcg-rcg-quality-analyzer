ccb50638880388f9fe5a5d2bc7930a7743b17855
Daniel Jönsson <dajo1903@student.miun.se>
2025-03-16 03:43:35 +0100

    fix: Fixed button cursor
    Fixed so that the cursor does not get stuck in pointer state after
    pressing the button.

--- core/src/main/java/com/mygdx/ui/GameButton.java
+++ core/src/main/java/com/mygdx/ui/GameButton.java
@@ -38,6 +38,12 @@ public class GameButton extends TextButton {
                 AudioManager.playSound("buttonClick");
                 return true;
             }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                super.touchUp(event, x, y, pointer, button);
+                Gdx.graphics.setSystemCursor(SystemCursor.Arrow);
+            }
         });
     }
 

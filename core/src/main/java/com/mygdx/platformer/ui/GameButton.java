6154b897bcf53295226385cf34014ba7f6fe6a5b
Daniel Jönsson <dajo1903@student.miun.se>
2025-03-16 20:45:50 +0100

    docs: Added JavaDoc for GameButton

--- core/src/main/java/com/mygdx/platformer/ui/GameButton.java
+++ core/src/main/java/com/mygdx/platformer/ui/GameButton.java
@@ -11,10 +11,77 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.mygdx.platformer.sound.AudioManager;
 import com.mygdx.platformer.sound.SoundType;
 
+/**
+ * A custom button implementation for the platformer game with enhanced
+ * interactivity.
+ * <p>
+ * This class extends LibGDX's TextButton to provide additional game-specific
+ * features:
+ * <ul>
+ * <li>Visual feedback when hovered (color change to red)</li>
+ * <li>Cursor changes to hand icon when hovered</li>
+ * <li>Sound effects for hover and click interactions</li>
+ * <li>Automatic cursor management</li>
+ * </ul>
+ * </p>
+ * <p>
+ * GameButton integrates with the AudioManager to play appropriate sound effects
+ * when the user interacts with the button, enhancing the user experience with
+ * audio feedback.
+ * </p>
+ * <p>
+ * Usage example:
+ * 
+ * <pre>
+ * // Create a button with text and skin
+ * GameButton startButton = new GameButton("Start Game", skin);
+ * 
+ * // Add click handler
+ * startButton.addListener(new ClickListener() {
+ *     public void clicked(InputEvent event, float x, float y) {
+ *         // Handle button click
+ *         game.startNewGame();
+ *     }
+ * });
+ * 
+ * // Add to stage
+ * stage.addActor(startButton);
+ * </pre>
+ * </p>
+ * 
+ * @see com.badlogic.gdx.scenes.scene2d.ui.TextButton
+ * @see com.mygdx.platformer.sound.AudioManager
+ * @author Robert Kullman
+ * @author Daniel Jönsson
+ */
 public class GameButton extends TextButton {
 
+    /**
+     * Flag indicating whether the button is currently being hovered over.
+     * <p>
+     * This state is used to determine when to change the button's color
+     * and is updated by the input listener's enter and exit methods.
+     * </p>
+     */
     private boolean hover;
 
+    /**
+     * Creates a new game button with the specified text and skin.
+     * <p>
+     * Initializes the button and sets up input listeners to handle hover effects,
+     * cursor changes, and sound playback. The button will automatically:
+     * <ul>
+     * <li>Change color to red when hovered</li>
+     * <li>Change the cursor to a hand icon when hovered</li>
+     * <li>Play a hover sound when the cursor enters the button</li>
+     * <li>Play a click sound when the button is pressed</li>
+     * <li>Reset the cursor when the button is no longer hovered or clicked</li>
+     * </ul>
+     * </p>
+     *
+     * @param text The text to display on the button
+     * @param skin The skin to use for the button's appearance
+     */
     public GameButton(String text, Skin skin) {
         super(text, skin);
         addListener(new InputListener() {
@@ -46,6 +113,17 @@ public class GameButton extends TextButton {
         });
     }
 
+    /**
+     * Returns the current color of the button based on hover state.
+     * <p>
+     * Overrides the default getColor method to change the button's color to red
+     * when it is being hovered over, providing visual feedback to the user.
+     * When not hovered, returns to the default color from the parent class.
+     * </p>
+     *
+     * @return Color.RED if the button is being hovered over, otherwise the default
+     *         color
+     */
     @Override
     public Color getColor() {
         return hover ? Color.RED : super.getColor();

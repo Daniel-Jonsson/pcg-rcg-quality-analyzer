717769a3ff8686631b1cc0ee1d4d8d23589800dc
Daniel Jönsson <dajo1903@student.miun.se>
2025-03-21 16:27:39 +0100

    Added package-info to sound package.

---
+++ core/src/main/java/com/mygdx/platformer/sound/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The package for sound-related classes and utilities.
+ * This package contains classes for managing audio playback, sound effects, and
+ * music.
+ * It provides functionality for loading, playing, and stopping sounds, as well
+ * as handling audio settings.
+ */
+package com.mygdx.platformer.sound;

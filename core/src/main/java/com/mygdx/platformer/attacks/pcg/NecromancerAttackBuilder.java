054e9337f634ac1bf1ae16c7a81a3e5f16f9b049
Daniel Jönsson <dajo1903@student.miun.se>
2025-03-28 11:04:03 +0100

    Added NecromancerAttackBuilder class.

---
+++ core/src/main/java/com/mygdx/platformer/attacks/pcg/NecromancerAttackBuilder.java
@@ -0,0 +1,34 @@
+package com.mygdx.platformer.attacks.pcg;
+
+public class NecromancerAttackBuilder implements Builder {
+
+    @Override
+    public void buildArc() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buildArc'");
+    }
+
+    @Override
+    public void buildSpeed() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buildSpeed'");
+    }
+
+    @Override
+    public void buildDamage() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buildDamage'");
+    }
+
+    @Override
+    public void buildProjectileNumber() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buildProjectileNumber'");
+    }
+
+    @Override
+    public void buildMovementPattern() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buildMovementPattern'");
+    }
+}

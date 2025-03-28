18b4fb648348a1e8d6c7cfef56f6a979804a0712
Daniel Jönsson <dajo1903@student.miun.se>
2025-03-28 11:02:31 +0100

    Made initial version of Builder interface.

---
+++ core/src/main/java/com/mygdx/platformer/attacks/pcg/Builder.java
@@ -0,0 +1,9 @@
+package com.mygdx.platformer.attacks.pcg;
+
+public interface Builder {
+    void buildArc();
+    void buildSpeed();
+    void buildDamage();
+    void buildProjectileNumber();
+    void buildMovementPattern();
+}

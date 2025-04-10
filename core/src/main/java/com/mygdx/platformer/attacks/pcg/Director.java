2ffed5251490cfef8c97f37441816750340bb40d
Daniel Jönsson <dajo1903@student.miun.se>
2025-04-10 11:39:22 +0200

    Fixed method to construct necromancer pcg attack.

--- core/src/main/java/com/mygdx/platformer/attacks/pcg/Director.java
+++ core/src/main/java/com/mygdx/platformer/attacks/pcg/Director.java
@@ -1,5 +1,14 @@
 package com.mygdx.platformer.attacks.pcg;
 
+import java.util.Random;
+
 public class Director {
-    
+    private final Random rand = new Random();
+
+    public void constructNecromancerPCGAttack(Builder attackBuilder) {
+        attackBuilder.setProjectileNumber(rand.nextInt(5));
+        // TODO: Make the damage and speed depend on current difficulty level
+        attackBuilder.setDamage(rand.nextInt(20, 30));
+        attackBuilder.setSpeed(rand.nextFloat(1, 3));
+    }
 }

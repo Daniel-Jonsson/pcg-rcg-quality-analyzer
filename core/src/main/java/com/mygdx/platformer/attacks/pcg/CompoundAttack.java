3d0b1a8fb6d34892be38db70fc27c4acd0738f71
Daniel Jönsson <dajo1903@student.miun.se>
2025-04-10 11:40:20 +0200

    Begun working on CompoundAttack, not finished.

--- core/src/main/java/com/mygdx/platformer/attacks/pcg/CompoundAttack.java
+++ core/src/main/java/com/mygdx/platformer/attacks/pcg/CompoundAttack.java
@@ -1,4 +1,28 @@
 package com.mygdx.platformer.attacks.pcg;
 
+import com.mygdx.platformer.attacks.BaseAttack;
+import com.mygdx.platformer.attacks.NecromancerAttack;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CompoundAttack {
+    private final int projectiles;
+    private final int damage;
+    private final float speed;
+    private final List<BaseAttack> attackPattern;
+
+    public CompoundAttack(int projectileNumber, float speed, int damage) {
+        projectiles = projectileNumber;
+        this.speed = speed;
+        this.damage = damage;
+        initializeAttacks();
+    }
+
+    private void initializeAttacks() {
+        attackPattern = new ArrayList<>();
+        for (int i = 0; i < projectiles; i++) {
+            attackPattern.add(new NecromancerAttack());
+        }
+    }
 }

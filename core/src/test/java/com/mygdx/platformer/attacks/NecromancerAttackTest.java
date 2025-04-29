1885ef8a73966f6a1916c85cf30d3bb845eb7471
Robert Kullman <roku0701@student.miun.se>
2025-04-29 18:12:44 +0200

    Add gradle dependencies for junit and add placeholder test class for testing NecromancerAttacks.

---
+++ core/src/test/java/com/mygdx/platformer/attacks/NecromancerAttackTest.java
@@ -0,0 +1,13 @@
+package com.mygdx.platformer.attacks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NecromancerAttackTemplateTest {
+
+    @Test
+    void dummyTest() {
+        assertTrue(true);
+    }
+}

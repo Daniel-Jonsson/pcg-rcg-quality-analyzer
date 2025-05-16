c09fce061796a4f7cd6a1c26a550cc02c55d6cf6
Daniel Jönsson <dajo1903@student.miun.se>
2025-05-16 13:07:18 +0200

    Added documentation for AttackModifier.

--- core/src/main/java/com/mygdx/platformer/attacks/modifiers/AttackModifier.java
+++ core/src/main/java/com/mygdx/platformer/attacks/modifiers/AttackModifier.java
@@ -2,7 +2,24 @@ package com.mygdx.platformer.attacks.modifiers;
 
 import com.mygdx.platformer.attacks.BaseAttack;
 
-
+/**
+ * Interface for defining attack modifier behaviors in the game.
+ * <p>
+ * Implementations of this interface can be attached to
+ * {@link com.mygdx.platformer.attacks.BaseAttack}
+ * objects to dynamically alter their properties or behavior during their
+ * lifecycle (e.g., adding
+ * effects such as pulsing, splitting, or changing speed).
+ * <p>
+ * Attack modifiers are used both in runtime gameplay and in code generation for
+ * analysis, allowing
+ * for flexible and extensible attack logic.
+ * </p>
+ *
+ * @author Daniel Jönsson
+ * @author Robert Kullman
+ * @see com.mygdx.platformer.attacks.BaseAttack
+ */
 public interface AttackModifier {
     abstract void update(BaseAttack attack);
     String getInlineLogicCode();

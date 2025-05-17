4c0c44a2af631bf229ab8e3e0a7964d625ee3b36
Daniel Jönsson <dajo1903@student.miun.se>
2025-05-17 11:06:40 +0200

    Added documentation for AccelerateMovement.

--- core/src/main/java/com/mygdx/platformer/attacks/movement/AccelerateMovement.java
+++ core/src/main/java/com/mygdx/platformer/attacks/movement/AccelerateMovement.java
@@ -3,7 +3,29 @@ package com.mygdx.platformer.attacks.movement;
 import com.mygdx.platformer.attacks.BaseAttack;
 import com.badlogic.gdx.math.Vector2;
 
-//TODO: check that the logic works as intended in game, I'm not sure it does
+/**
+ * A {@link MovementPatternBehavior} that causes an attack to accelerate or
+ * decelerate horizontally based on its current speed and position.
+ * <p>
+ * The acceleration logic increases the attack's speed in stages, with different
+ * scaling factors depending on the current speed and vertical position.
+ * If the attack is moving slowly, it accelerates linearly; at moderate speeds,
+ * it accelerates more rapidly if above a certain height (y > 50), otherwise
+ * less so.
+ * If the speed exceeds a threshold, the attack decelerates. The direction of
+ * movement is preserved throughout the update.
+ * </p>
+ *
+ * <h2>Usage</h2>
+ * 
+ * <pre>
+ * // Attach to an attack to make it accelerate horizontally
+ * attack.setMovementBehavior(new AccelerateMovement());
+ * </pre>
+ *
+ * @see MovementPatternBehavior
+ * @see com.mygdx.platformer.attacks.BaseAttack
+ */
 public class AccelerateMovement implements MovementPatternBehavior {
     @Override
     public void update(BaseAttack attack) {

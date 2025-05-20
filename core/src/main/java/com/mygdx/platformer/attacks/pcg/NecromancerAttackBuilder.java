d9b3703e6bc7350b2e255cc602afbd5447d97198
Daniel Jönsson <dajo1903@student.miun.se>
2025-05-20 12:10:18 +0200

    Added Javadoc for NecromancerAttackBuilder

--- core/src/main/java/com/mygdx/platformer/attacks/pcg/NecromancerAttackBuilder.java
+++ core/src/main/java/com/mygdx/platformer/attacks/pcg/NecromancerAttackBuilder.java
@@ -1,12 +1,21 @@
 package com.mygdx.platformer.attacks.pcg;
 
+/**
+ * The NecromancerAttackBuilder class implements the Builder interface and
+ * provides methods to set the number of projectiles, speed, and damage for an attack.
+ * 
+ * @author Daniel Jönsson
+ * @author Robert Kullman
+ */
 public class NecromancerAttackBuilder implements Builder {
     private int projectiles;
     private float speed;
     private int damage;
 
     /**
+     * Sets the speed of the attack.
      *
+     * @param speed The speed to set.
      */
     @Override
     public void setSpeed(float speed) {
@@ -14,7 +23,9 @@ public class NecromancerAttackBuilder implements Builder {
     }
 
     /**
+     * Sets the damage of the attack.
      *
+     * @param damage The damage to set.
      */
     @Override
     public void setDamage(int damage) {
@@ -22,13 +33,20 @@ public class NecromancerAttackBuilder implements Builder {
     }
 
     /**
+     * Sets the number of projectiles for the attack.
      *
+     * @param projectileNumber The number of projectiles to set.
      */
     @Override
     public void setProjectileNumber(int projectileNumber) {
         this.projectiles = projectileNumber;
     }
 
+    /**
+     * Returns a new CompoundAttack object with the specified number of projectiles, speed, and damage.
+     *
+     * @return A new CompoundAttack object.
+     */
     public CompoundAttack getResult() {
         return new CompoundAttack(projectiles, speed, damage);
     }
